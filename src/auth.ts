876386ab28f4e7642b5a460668bace36a27cba22
torressam333 <torressam333@gmail.com>
2023-12-27 11:38:14 -0700

    Descructure exports from next auth constructor

--- src/auth.ts
+++ src/auth.ts
@@ -9,12 +9,26 @@ const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET)
   throw new Error("Missing GitHub oauth credentials");
 
-export default NextAuth({
+// @see NextAuth constructor for all destructurable properties
+export const {
+  handlers: { GET, POST },
+  auth,
+  signOut,
+  signIn
+} = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
     GitHubProvier({
       clientId: GITHUB_CLIENT_ID,
       clientSecret: GITHUB_CLIENT_SECRET
     })
-  ]
+  ],
+  callbacks: {
+    // Bug for next-auth
+    async session({ session, user }: any) {
+      if (session && user) session.user.id = user.id;
+
+      return session;
+    }
+  }
 });
