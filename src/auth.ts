993ad9cabc8e93f97a1a25c6490323ebafdf9ba5
torressam333 <torressam333@gmail.com>
2023-12-27 11:32:33 -0700

    Add error handling for missing oauth credentials

--- src/auth.ts
+++ src/auth.ts
@@ -3,12 +3,18 @@ import GitHubProvier from "next-auth/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "./db";
 
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+
+if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET)
+  throw new Error("Missing GitHub oauth credentials");
+
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
     GitHubProvier({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET
+      clientId: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET
     })
   ]
 });
