8b67cb366f6e86314c5e5e19ba07b167acd04d47
torressam333 <torressam333@gmail.com>
2023-12-31 14:11:33 -0700

    Use NextAuth to sign in and out using GitHub

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
 import { Button } from "@nextui-org/react";
+import * as actions from "@/actions";
+
+type AuthButtonProps = {
+  action: () => Promise<void>;
+  buttonText: string;
+};
+
+const AuthButton = ({ action, buttonText }: AuthButtonProps) => {
+  return (
+    <form action={action}>
+      <Button type="submit">{buttonText}</Button>
+    </form>
+  );
+};
+
 export default function HomePage() {
   return (
     <main>
-      <h1>Hiiiii</h1>
-      <Button>Click Me</Button>
+      <AuthButton action={actions.signIn} buttonText="Sign In" />
+      <AuthButton action={actions.signOut} buttonText="Sign Out" />
     </main>
   );
 }
