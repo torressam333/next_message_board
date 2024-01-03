6d22ebde8afdba483356eda235c6174a6f7d117b
torressam333 <torressam333@gmail.com>
2024-01-03 11:22:00 -0700

    Wire up creat topic form with relevant server action

--- src/components/topics/TopicCreateForm.tsx
+++ src/components/topics/TopicCreateForm.tsx
@@ -6,6 +6,7 @@ import {
   Input,
   Textarea
 } from "@nextui-org/react";
+import * as actions from "@/actions";
 
 const TopicCreateForm = () => {
   return (
@@ -15,7 +16,7 @@ const TopicCreateForm = () => {
           <Button color="primary">Create Topic</Button>
         </PopoverTrigger>
         <PopoverContent>
-          <form>
+          <form action={actions.createTopic}>
             <div className="flex flex-col flex-wrap md:flex-nowrap gap-4 p-4 w-80">
               <h3 className="text-lg">Create a Topic</h3>
               <Input placeholder="Name" label="Name" labelPlacement="outside" />
